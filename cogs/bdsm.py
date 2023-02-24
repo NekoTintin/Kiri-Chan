a94bfc1a9de0ceb0dc922cb3d39f3d2a1eb4a808
Tintin361 <tintin361official@gmail.com>
2023-02-24 04:21:11 +0100

    v1.2.6

--- cogs/bdsm.py
+++ cogs/bdsm.py
@@ -12,7 +12,7 @@ class Bdsm(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
         bot.loop.create_task(self.bdsm())
-        #bot.loop.create_task(self.marie())
+        bot.loop.create_task(self.marie())
         
     async def bdsm(self):
         await self.bot.wait_until_ready()
