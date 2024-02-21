a0f56a853a0a95a3456b5c9b13407362dea38264
Quentin <tintin361official@gmail.com>
2024-02-21 09:28:41 +0100

    3.3.5

---
+++ tools/paths.py
@@ -0,0 +1,6 @@
+cogs_path = "/home/tintin/discord_bot/Kiri-chan/cogs/"
+rpg_path = "/home/tintin/discord_bot/Kiri-chan/rpg_cogs/"
+data_path = "/home/tintin/discord_bot/Kiri-chan/data/"
+db_path = "/home/tintin/discord_bot/Kiri-chan/data/user_data.db"
+
+nekobot_path = "/home/tintin/discord_bot/NekoBot/"
