5f906a9fe07108b474ff2d68724ddcf1b16c6fb7
Quentin <30444189+Tintin361@users.noreply.github.com>
2024-02-25 05:00:13 +0100

    Delete tools/paths.py

--- tools/paths.py
+++
@@ -1,9 +0,0 @@
-cogs_path = "/home/tintin/discord_bot/Kiri-chan/cogs/"
-rpg_path = "/home/tintin/discord_bot/Kiri-chan/rpg_cogs/"
-data_path = "/home/tintin/discord_bot/Kiri-chan/data/"
-db_path = "/home/tintin/discord_bot/Kiri-chan/data/user_data.db"
-
-nekobot_path = "/home/tintin/discord_bot/NekoBot/"
-nekodata_path = "/home/tintin/discord_bot/NekoBot/data/"
-
-web_path = "/var/www/html/images/"
